b5d3fc96143fe97bb6573aedbf6727a494de15ff
Chanison Aupathin <89581992+ChanisonM@users.noreply.github.com>
2023-05-02 22:17:19 +0700

    chanison

--- js/index.js
+++ js/index.js
@@ -33,20 +33,28 @@ var lastName = [
   "Batz",
 ];
 
+
+document.querySelector('.title').innerText = `How many times would you like to generator (up to 20)`
+
 function func_getName(){
-    var randomName = firstName[Math.floor(Math.random() * firstName.length)] + ' ' + lastName[Math.floor(Math.random() * lastName.length)]
+    var randomName = `${firstName[Math.floor(Math.random() * firstName.length)]}  ${lastName[Math.floor(Math.random() * lastName.length)]}`
     return randomName
 }
 
 function func_nameQty(){
+
+    var MaxlengthNanme = 20
+
     var userQty = document.querySelector('.qty').value;
     document.querySelector('.nameList').innerHTML = ""
-    if(userQty > 10){
-        document.querySelector('.nameList').innerHTML = "ONLY 10 NAME CAN BE GENERATE"
+    if(userQty > MaxlengthNanme){
+        document.querySelector('.nameList').innerHTML = `ONLY ${MaxlengthNanme} NAME CAN BE GENERATE`
     }else{
         for (let i = 0; i < userQty; i++) {
             document.querySelector('.nameList').innerHTML += func_getName() + '<br>'
             
         }
     }
+
+    
 }
